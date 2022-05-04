dc16d124745c0dcac3edcac9eced059f6f958b0d
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-04 20:59:54 +0100

    ✨ BUILD: Added "Registro" component

--- src/components/Registro/Registro.tsx
+++ src/components/Registro/Registro.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
-function Registro() {
+import './Registro.css'
+function Registro():JSX.Element {
   return (
-	<div>Registro</div>
+	<div className='containerForm'>
+	<form action="" className="formStyle">
+
+		<input id='name' name='name' type="text" placeholder='Nombre' required size={30} maxLength={60}/>
+
+
+		<input id='surname' name='surname' type="text" placeholder='Apellidos' />
+
+		<input type="email" name="email" id="email" placeholder='Correo Electrónico' required />
+
+		<input type="text" name='address1' id='address1' placeholder='Dirección línea 1' required/>
+		<input type="text" name='address2' id='address2' placeholder='Dirección línea 2' required />
+
+		<input type="password" name="password" id='password' placeholder='Contraseña'/>
+
+		<button type='submit'>Regitrarse</button>
+	</form>
+	</div>
   )
 }
 
