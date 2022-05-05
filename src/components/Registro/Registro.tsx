b8ec9ed62ed08768373c0cea1ef63976403ad67f
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-05 11:28:12 +0100

    💄 STYLES: Added input for building and edited input for address

--- src/components/Registro/Registro.tsx
+++ src/components/Registro/Registro.tsx
@@ -1,22 +1,22 @@
-import './Registro.css'
+import "./Registro.css"
 function Registro():JSX.Element {
   return (
-	<div className='containerForm'>
+	<div className="containerForm">
 	<form action="" className="formStyle">
 
-		<input id='name' name='name' type="text" placeholder='Nombre' required size={30} maxLength={60}/>
+		<input id="name" name="name" type="text" placeholder="Nombre" required size={30} maxLength={60}/>
 
+		<input id="surname" name="surname" type="text" placeholder="Apellidos" />
 
-		<input id='surname' name='surname' type="text" placeholder='Apellidos' />
+		<input type="email" name="email" id="email" placeholder="Correo Electrónico" required />
 
-		<input type="email" name="email" id="email" placeholder='Correo Electrónico' required />
+		<input type="text" name="address" id="address" placeholder="Dirección" required/>
 
-		<input type="text" name='address1' id='address1' placeholder='Dirección línea 1' required/>
-		<input type="text" name='address2' id='address2' placeholder='Dirección línea 2' required />
+		<input type="text" name="building" id="building" placeholder="Edificio" />
 
-		<input type="password" name="password" id='password' placeholder='Contraseña'/>
+		<input type="password" name="password" id="password" placeholder="Contraseña"/>
 
-		<button type='submit'>Registrarse</button>
+		<button type="submit">Registrarse</button>
 	</form>
 	</div>
   )
