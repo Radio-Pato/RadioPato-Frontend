b97afbae0064b8c275c5c48dc74dbc517a64d163
Maria <mariaochandocalvo@gmail.com>
2022-05-06 13:37:20 +0100

    💄 ADD css to LoadPageComponent

--- src/components/LoadingPageComponent/LoadingPageComponent.tsx
+++ src/components/LoadingPageComponent/LoadingPageComponent.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import LoadComponent from '../LoadComponent/LoadComponent'
-import imagenes from '../../assets/imagenes';
+import React from "react";
+import LoadComponent from "../LoadComponent/LoadComponent";
+import imagenes from "../../assets/imagenes";
+import "./LoadingPageComponent.css";
 
 function LoadingPageComponent() {
   return (
-	<div>
-        <img src={imagenes.Logo960.src} alt={imagenes.Logo960.alt} className=""/>
-
-        <h1>Radio Pato</h1>
-        <LoadComponent></LoadComponent>
+    <div className="bigcontainer">
+      <div className="container">
+        <img
+          src={imagenes.Logo480.src}
+          alt={imagenes.Logo480.alt}
+          className="logo"
+        />
+        <div className="smallcontainer">
+          <h1 className="title">Radio Pato</h1>
+          <h3 className="slogan">
+            Tu aplicación Web para gestionar tu comunidad de vecinos
+          </h3>
+        </div>
+        <LoadComponent />
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoadingPageComponent
+export default LoadingPageComponent;
