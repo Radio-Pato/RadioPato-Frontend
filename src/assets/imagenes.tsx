36f643ba3d74c89a54785800f299e2cece4f435b
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-05-08 19:05:24 +0100

    🥚 Favicon

--- src/assets/imagenes.tsx
+++ src/assets/imagenes.tsx
@@ -2,6 +2,8 @@ import Logo_960 from './img/Logo_Radio_Pato_960.png';
 import Logo_768 from './img/Logo_Radio_Pato_768.png';
 import Logo_480 from './img/Logo_Radio_Pato_480.png';
 import Logo_192 from './img/Logo_Radio_Pato_192.png';
+import Logo_96 from './img/Logo_Radio_Pato_96.png';
+import Logo_68 from './img/Logo_Radio_Pato_68.png';
 import Access from './img/Access.jpg';
 import Register from './img/Register.jpg';
 
@@ -21,6 +23,14 @@ export default {
     Logo192: {
         alt: "Logo_Radio_Pato_192",
         src: Logo_192
+    },
+    Logo96: {
+        alt: "Logo_Radio_Pato_96",
+        src: Logo_96
+    },
+    Logo68: {
+        alt: "Logo_Radio_Pato_68",
+        src: Logo_68
     },
     Access: {
         alt: "Imagen de Access",
