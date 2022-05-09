8211815b017d3478e31fb069f22b9d9f543f276d
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-05-09 12:54:16 +0100

    💄 Assets

---
+++ src/Assets/imagenes.tsx
@@ -0,0 +1,43 @@
+import Logo_960 from './img/Logo_Radio_Pato_960.png';
+import Logo_768 from './img/Logo_Radio_Pato_768.png';
+import Logo_480 from './img/Logo_Radio_Pato_480.png';
+import Logo_192 from './img/Logo_Radio_Pato_192.png';
+import Logo_96 from './img/Logo_Radio_Pato_96.png';
+import Logo_68 from './img/Logo_Radio_Pato_68.png';
+import Access from './img/Access.jpg';
+import Register from './img/Register.jpg';
+
+export default {
+    Logo960: {
+        alt: "Logo_Radio_Pato_960",
+        src: Logo_960
+    },
+    Logo768: {
+        alt: "Logo_Radio_Pato_768",
+        src: Logo_768
+    },
+    Logo480: {
+        alt: "Logo_Radio_Pato_480",
+        src: Logo_480
+    },
+    Logo192: {
+        alt: "Logo_Radio_Pato_192",
+        src: Logo_192
+    },
+    Logo96: {
+        alt: "Logo_Radio_Pato_96",
+        src: Logo_96
+    },
+    Logo68: {
+        alt: "Logo_Radio_Pato_68",
+        src: Logo_68
+    },
+    Access: {
+        alt: "Imagen de Access",
+        src: Access
+    },
+    Register: {
+        alt: "Iamgen de Register",
+        src: Register
+    }
+}
