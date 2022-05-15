32fb6b540e8404d209f33a1d0b81c2859f5d3b46
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-05-15 16:59:49 +0100

    💄 Update Tabs Styles

--- src/components/BoxCommentComponent/BoxCommentComponent.tsx
+++ src/components/BoxCommentComponent/BoxCommentComponent.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 import styles from './BoxCommentComponent.module.css'
 
 function BoxCommentComponent() {
-  return (
-      <>
+
+    return (
+        <>
+        <div className={styles.tabs__container}>
+            <a href="#"  className={`${styles.tab} ${styles.active}`}>General</a>
+            <a href="#"  className={styles.tab}>Pestaña 1</a>
+            <a href="#"  className={styles.tab}>Pestaña 2</a>
+        </div>
         <div className={styles.comments__container}>
             <div className={styles.comment}>
-              <p>Esto es un comentario.</p>
+            <p>Esto es un comentario.</p>
             </div>
             <div className={styles.comment}>
-              <p>Esto es un comentario.</p>
+            <p>Esto es un comentario.</p>
             </div>
             <div className={styles.comment}>
-              <p>Esto es un comentario.</p>
+            <p>Esto es un comentario.</p>
             </div>
             <div className={styles.comment}>
-              <p>Esto es un comentario.</p>
+            <p>Esto es un comentario.</p>
             </div>
         </div>
-      </>
-  )
+        </>
+    )
 }
 
 
