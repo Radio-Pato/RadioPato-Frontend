d0fb4c887a67722000ec0d48a1c9833575d6c18b
AlejandroVelazquez1995 <alejandro_rac_13@hotmail.com>
2022-05-15 17:25:39 +0100

    ✅ Tabs can toggle

--- src/components/BoxCommentComponent/BoxCommentComponent.tsx
+++ src/components/BoxCommentComponent/BoxCommentComponent.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './BoxCommentComponent.module.css'
 
 function BoxCommentComponent() {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index: number) => {
+        setToggleState(index);
+    };
 
     return (
         <>
         <div className={styles.tabs__container}>
-            <a href="#"  className={`${styles.tab} ${styles.active}`}>General</a>
-            <a href="#"  className={styles.tab}>Pestaña 1</a>
-            <a href="#"  className={styles.tab}>Pestaña 2</a>
+            <a href="#" onClick={() => toggleTab(1)} className={toggleState === 1 ? `${styles.tab} ${styles.active}` : styles.tab}>General</a>
+            <a href="#" onClick={() => toggleTab(2)} className={toggleState === 2 ? `${styles.tab} ${styles.active}` : styles.tab}>Pestaña 1</a>
+            <a href="#" onClick={() => toggleTab(3)} className={toggleState === 3 ? `${styles.tab} ${styles.active}` : styles.tab}>Pestaña 2</a>
         </div>
         <div className={styles.comments__container}>
             <div className={styles.comment}>
