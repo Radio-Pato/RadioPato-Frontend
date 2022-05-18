453a7fa746b19bf4c0e333673bc807f2c2f35e11
Maria <mariaochandocalvo@gmail.com>
2022-05-18 22:24:33 +0100

    💩 UPDATE user

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React, { Children, useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import styles from "./Profile.module.css";
 import { useForm } from "react-hook-form";
-import { getUsers } from "../../utils/Services";
+import { getUsers, updateUser } from "../../utils/Services";
 import Cookies from "js-cookie";
 
 type TInputs = {
@@ -24,7 +24,18 @@ function Profile() {
     getUsers(email, token).then((res) => {
       setUser(res.data.data);
     });
-  },[]);
+  }, [user]);
+
+  // const [takeAway, setTakeAway] = useState({ takeAway: false, direction: "" })
+
+  // const handleInputChange = (event:any) => {
+  //   const target = event.target
+  //   if (target.name === "takeAway") {
+  //     setTakeAway({ takeAway: target.checked, direction: "" })
+  //   } else {
+  //     setTakeAway({ direction: target.value, takeAway: true })
+  //   }
+  // }
 
   const {
     register,
@@ -33,105 +44,112 @@ function Profile() {
   } = useForm({
     reValidateMode: "onChange",
   });
-
-  const onSubmit = async (data: any) => {
-    console.log(data);
+  const [name, setName] = useState("nombre");
+  const handleChange = (event: any) => {
+    setName(event.target.value);
   };
-console.log(typeof user)
+  const onSubmit = async (data: any) => {
+    updateUser(data);
+    console.log(data);
+    console.log(updateUser);
+  };
+
   if (user <= 0) {
     return <p>Loading...</p>;
   } else {
     return (
       <Layout>
-		<>
-		  <p>{user.email}</p>
-        <div className="form">
-          <form onSubmit={handleSubmit(onSubmit)} className="formStyle">
-            <input
-              type="text"
-              id="name"
-              placeholder="Nombre"
-			  value={user.name}
-              {...register("name", {
-                required: true,
-                maxLength: 60,
-              })}
-            />
-            {/* Errors */}
-            {errors.name?.type === "required" && (
-              <div className="errors">Debe introducir un Nombre</div>
-            )}
-            {errors.name?.type === "maxLength" && (
-              <div className="errors">
-                El nombre no puede contener mas 60 carácteres
-              </div>
-            )}
-            <input
-              type="text"
-              id="surname"
-              placeholder="Apellidos"
-			  value={user.surname}
-              {...register("surname", {
-                required: true,
-                maxLength: 120,
-              })}
-            />
-            {/* Errors */}
-            {errors.surname?.type === "required" && (
-              <div className="errors">Debe introducir al menos un apellido</div>
-            )}
-            {errors.surname?.type === "maxLength" && (
-              <div className="errors">
-                Los apellidos no pueden contener más de 120 carácteres
-              </div>
-            )}
+        <>
+          <p>{user.email}</p>
+          <div className="form">
+            <form onSubmit={handleSubmit(onSubmit)} className="formStyle">
+              <input
+                type="text"
+                id="name"
+                placeholder={user.name}
+                {...register("name", {
+                  required: true,
+                  maxLength: 60,
+                })}
+              />
+              {/* Errors */}
+              {errors.name?.type === "required" && (
+                <div className="errors">Debe introducir un Nombre</div>
+              )}
+              {errors.name?.type === "maxLength" && (
+                <div className="errors">
+                  El nombre no puede contener mas 60 carácteres
+                </div>
+              )}
+              <input
+                type="text"
+                id="surname"
+                placeholder="Apellidos"
+                value={user.surname}
+                {...register("surname", {
+                  required: true,
+                  maxLength: 120,
+                })}
+              />
+              {/* Errors */}
+              {errors.surname?.type === "required" && (
+                <div className="errors">
+                  Debe introducir al menos un apellido
+                </div>
+              )}
+              {errors.surname?.type === "maxLength" && (
+                <div className="errors">
+                  Los apellidos no pueden contener más de 120 carácteres
+                </div>
+              )}
 
+              <input
+                type="text"
+                id="address"
+                placeholder="Dirección"
+                value={user.address}
+                {...register("address", {
+                  required: true,
+                  maxLength: 200,
+                })}
+              />
+              {/* Errors */}
+              {errors.address?.type === "required" && (
+                <div className="errors">Debe introducir su dirección</div>
+              )}
+              {errors.address?.type === "maxLength" && (
+                <div className="errors">
+                  La dirección debe tener entre 5 y 200 carácteres
+                </div>
+              )}
 
-
-            <input
-              type="text"
-              id="address"
-              placeholder="Dirección"
-			  value={user.address}
-              {...register("address", {
-                required: true,
-                maxLength: 200,
-              })}
-            />
-            {/* Errors */}
-            {errors.address?.type === "required" && (
-              <div className="errors">Debe introducir su dirección</div>
-            )}
-            {errors.address?.type === "maxLength" && (
-              <div className="errors">
-                La dirección debe tener entre 5 y 200 carácteres
-              </div>
-            )}
-
-            <input
-              type="text"
-              id="building"
-              placeholder="Edificio"
-			  value={user.building}
-              {...register("building", {
-                required: true,
-                maxLength: 100,
-              })}
-            />
-            {errors.building?.type === "required" && (
-              <div className="errors">
-                Debe introducir el edificio donde reside
-              </div>
-            )}
-            {errors.building?.type === "maxLength" && (
-              <div className="errors">
-                El nombre del edificio no puede contener mas 100 carácteres
-              </div>
-            )}
-            <button type="submit">Editar</button>
+              <input
+                type="text"
+                id="building"
+                placeholder="Edificio"
+                value={user.building}
+                {...register("building", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.building?.type === "required" && (
+                <div className="errors">
+                  Debe introducir el edificio donde reside
+                </div>
+              )}
+              {errors.building?.type === "maxLength" && (
+                <div className="errors">
+                  El nombre del edificio no puede contener mas 100 carácteres
+                </div>
+              )}
+              <button type="submit">Editar</button>
+            </form>
+          </div>
+          <form>
+            <input onChange={handleChange} value={user.name} type="text" />
           </form>
-        </div>
-		</>
+        </>
       </Layout>
     );
   }
--- src/utils/Services.tsx
+++ src/utils/Services.tsx
@@ -1,28 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-let URL_MAIN= "http://localhost:3001/"
+let URL_MAIN = "http://localhost:3001/";
 
+export const postRegister = async (data: any) => {
+  let URL = `${URL_MAIN}usuarios/registro`;
+  let response = await axios.post(URL, data);
+  return response;
+};
 
-export const postRegister = async (data:any) => {
-    let URL = `${URL_MAIN}usuarios/registro`;
-    let response = await axios.post(URL, data);
-    return response;
-  };
+export const getRegister = async (data: any) => {
+  let URL = `${URL_MAIN}usuarios/acceso`;
+  let response = await axios.post(URL, data);
+  return response;
+};
 
-  export const getRegister = async (data:any) => {
-	let URL = `${URL_MAIN}usuarios/acceso`;
-	let response = await axios.post(URL,data)
-    return response;
-  };
+export const getSections = async () => {
+  let URL = `${URL_MAIN}secciones`;
+  let response = await axios.get(URL);
+  return response;
+};
 
-  export const getSections = async () =>{
-	  let URL = `${URL_MAIN}secciones`;
-	  let response = await axios.get(URL)
-	  return response
-  }
+export const getUsers = async (
+  email: String | undefined,
+  token: String | undefined
+) => {
+  let URL = `${URL_MAIN}usuarios/miperfil`;
+  let response = await axios.post(URL, { email: email, token: token });
+  return response;
+};
 
-  export const getUsers = async (email:(String|undefined), token:(String|undefined)) =>{
-	  let URL = `${URL_MAIN}usuarios/miperfil`
-	  let response = await axios.post(URL,{email: email, token:token})
-	  return response
-  }
+export const updateUser = async (data: any) => {
+  let URL = `${URL_MAIN}usuarios/miperfil`;
+  let response = await axios.patch(URL, data);
+  return response;
+};
