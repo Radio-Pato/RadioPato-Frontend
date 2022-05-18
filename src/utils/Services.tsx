ae8ccdd4c9c222f424c50d76de6b2a663241c2ef
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-05-18 19:23:09 +0100

    🐛 Added CR7

--- src/utils/Services.tsx
+++ src/utils/Services.tsx
@@ -1,16 +1,28 @@
 import axios from 'axios';
 
-let URL_MAIN= "http://localhost:3001/usuarios/"
+let URL_MAIN= "http://localhost:3001/"
 
 
 export const postRegister = async (data:any) => {
-    let URL = `${URL_MAIN}registro`;
+    let URL = `${URL_MAIN}usuarios/registro`;
     let response = await axios.post(URL, data);
     return response;
   };
 
   export const getRegister = async (data:any) => {
-	let URL = `${URL_MAIN}acceso`;
+	let URL = `${URL_MAIN}usuarios/acceso`;
 	let response = await axios.post(URL,data)
     return response;
   };
+
+  export const getSections = async () =>{
+	  let URL = `${URL_MAIN}secciones`;
+	  let response = await axios.get(URL)
+	  return response
+  }
+
+  export const getUsers = async (email:(String|undefined), token:(String|undefined)) =>{
+	  let URL = `${URL_MAIN}usuarios/miperfil`
+	  let response = await axios.post(URL,{email: email, token:token})
+	  return response
+  }
